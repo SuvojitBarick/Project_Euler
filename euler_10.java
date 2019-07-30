0693ce56c79cf90f04c10a3f4b4f2d9033c251f4
Cool-Rahul <53303788+Cool-Rahul@users.noreply.github.com>
2019-07-30 19:14:18 +0530

    Update euler_10.java

--- euler_10.java
+++ euler_10.java
@@ -4,7 +4,7 @@ public class euler_10
             {
         	        if (n < 2) return false;
         	        else if (n == 2) return true;
-        	        for (int i = 2; i < Math.pow(n, 0.5) + 1; i++)
+        	        for (int i = 2; i < Math.sqrt(n) + 1; i++)
         	            if (n % i == 0)
         	                return false;
         	        return true;
